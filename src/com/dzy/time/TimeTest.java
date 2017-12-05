0244b8bc4bdd6c31a0996948a6c6ad1434680f2d
yin2na <yin2na@gmail.com>
2017-12-06 04:13:13 +0800

    test

---
+++ src/com/dzy/time/TimeTest.java
@@ -0,0 +1,16 @@
+package com.dzy.time;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello\nworld"+2);
+	}
+
+}
